a37b7ca3e8f487d973b88625e7fa4220930e38c7
Daniel Soto <dasg.1993@gmail.com>
2022-02-28 17:41:26 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+  Button,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola</Text>
+      <Button
+        title="Hola"
+        color="#841584"
+        onPress={() =>  null}
+        />
+    </View>
+  );
+};
+
+
+export default App;
