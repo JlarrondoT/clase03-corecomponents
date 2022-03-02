9c5925211eab6e3c82060f567b63b526f4a61fc5
Daniel Soto <dasg.1993@gmail.com>
2022-03-02 17:37:45 -0500

    [feature] - add state in app & style elements

--- App.js
+++ App.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   View,
   Button,
-  TextInput
+  TextInput,
+  Text
 } from 'react-native';
 
 import { styles } from './styles';
 
 
 const App = () => {
+  const [text, setText] = useState('');
+  const [textList, setTextList] = useState([
+    {
+      id: 1,
+      value: 'buy some water'
+    }
+  ]);
   return (
     <View 
       style={styles.container}
     >
-      <TextInput
-        placeholder='Type here'
-        autoCorrect={false}
-        autoFocus={true}
-        placeholderTextColor='#841584'
-        style={styles.textInput}
-      />
-      <Button
-        title="Add"
-        color="#841584"
-        onPress={() =>  null}
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder='Type here'
+          autoCorrect={false}
+          autoFocus={true}
+          placeholderTextColor='#841584'
+          style={styles.textInput}
+          value={text}
         />
+        <Button
+          title="Add"
+          color="#841584"
+          onPress={() =>  null}
+          />
+      </View>
+      <View style={styles.containerList}>
+        {textList.map(item  => (
+          <Text style={styles.textList}>{item.value}</Text>
+        ))}
+      </View>
     </View>
   );
 };
--- styles.js
+++ styles.js
@@ -1,13 +1,18 @@
 
 import { StyleSheet } from 'react-native';
 
-
 export const styles = StyleSheet.create({
     container: {
+      flex: 1,
+    },
+    containerInput: {
       padding: 25,
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
+    },
+    containerList: {
+      paddingHorizontal: 25,
     },
     textInput: {
       borderColor: 'black',
@@ -17,5 +22,10 @@ export const styles = StyleSheet.create({
       height: 40,
       fontSize: 14,
       color: '#212121'
+    },
+    textList: {
+      fontSize: 14,
+      color: '#212121',
+      marginBottom: 10,
     }
 });
